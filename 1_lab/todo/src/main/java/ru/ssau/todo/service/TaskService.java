8486642b933501923df883b2de48a7e2e14909ba
GelKod <lokod235@gmail.com>
2026-04-04 06:37:00 +0400

    Done 3lab, but no tests

--- 1_lab/todo/src/main/java/ru/ssau/todo/service/TaskService.java
+++ 1_lab/todo/src/main/java/ru/ssau/todo/service/TaskService.java
@@ -1,69 +1,98 @@
 package ru.ssau.todo.service;
 
 import org.springframework.stereotype.Service;
+
+import ru.ssau.todo.dto.TaskDto;
 import ru.ssau.todo.entity.Task;
 import ru.ssau.todo.entity.TaskStatus;
+import ru.ssau.todo.entity.User;
+import ru.ssau.todo.exception.TaskNotFoundException;
 import ru.ssau.todo.repository.TaskRepository;
+import ru.ssau.todo.repository.UserRepository;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 // TODO: Lab 3 - migrate to JPA
 @Service
 public class TaskService {
 
     private final TaskRepository taskRepository;
+    private final UserRepository userRepository;
 
-    public TaskService(TaskRepository taskRepository) {
+    public TaskService(TaskRepository taskRepository, UserRepository userRepository) {
         this.taskRepository = taskRepository;
+        this.userRepository = userRepository;
     }
 
-    public Task create(Task task) {
-        long userId = task.getCreatedBy();
-        long activeCount = taskRepository.countActiveTasksByUserId(userId);
+    public TaskDto create(TaskDto taskDto) {
+        Optional<User> optUser = userRepository.findById(taskDto.getCreatedBy());
+        if(optUser.isEmpty()){
+            throw new IllegalStateException("Пользователь не найден");
+        }
+        User user = optUser.get();
+
+        long activeCount = taskRepository.countActiveTasksByUserId(user.getId());
         if (activeCount >= 10) {
             throw new IllegalStateException("У пользователя больше 10 активных задач");
         }
-        if (task.getStatus() == null) {
-            task.setStatus(TaskStatus.OPEN);
+        if (taskDto.getStatus() == null) {
+            taskDto.setStatus(TaskStatus.OPEN);
         }
 
-        return taskRepository.create(task);
+        Task task = new Task(taskDto.getTitle(), user, taskDto.getStatus());
+        taskRepository.save(task);
+        return taskDto;
     }
 
-    public Optional<Task> findById(long id) {
-        return taskRepository.findById(id);
+    public Optional<TaskDto> findById(long id) {
+        return taskRepository.findById(id).map(this::toDto);
     }
 
-    public List<Task> findAll(LocalDateTime from, LocalDateTime to, long userId) {
-        return taskRepository.findAll(from, to, userId);
+    public List<TaskDto> findAll(LocalDateTime from, LocalDateTime to, long userId) {
+        return taskRepository.findAll(userId, from, to)
+                .stream()
+                .map(this::toDto)
+                .collect(Collectors.toList());
     }
 
-    public void updateTask(Task task) throws Exception {
-        Optional<Task> existingTaskOpt = taskRepository.findById(task.getId());
+    public void updateTask(TaskDto task) throws TaskNotFoundException {
+        Optional<TaskDto> existingTaskOpt = taskRepository.findById(task.getId()).map(this::toDto);
+        User user;
         if (existingTaskOpt.isPresent()) {
-            Task existing = existingTaskOpt.get();
+            TaskDto existing = existingTaskOpt.get();
             boolean wasActive = existing.getStatus() == TaskStatus.OPEN || existing.getStatus() == TaskStatus.IN_PROGRESS;
             boolean isActive = task.getStatus() == TaskStatus.OPEN || task.getStatus() == TaskStatus.IN_PROGRESS;
 
+            if(userRepository.findById(existing.getId()).isEmpty()){
+                throw new TaskNotFoundException(existing.getId());
+            }
+            user = userRepository.findById(existing.getId()).get();
+
             if (!wasActive && isActive) {
-                long activeCount = taskRepository.countActiveTasksByUserId(task.getCreatedBy());
+                long activeCount = taskRepository.countActiveTasksByUserId(existing.getCreatedBy());
                 if (activeCount >= 10) {
                     throw new IllegalStateException("У пользователя больше 10 активных задач");
                 }
             }
         }
-        taskRepository.update(task);
+        else{
+            throw new TaskNotFoundException(task.getId());
+        }
+        taskRepository.save(new Task(existingTaskOpt.get().getTitle(), user,existingTaskOpt.get().getStatus()));
     }
 
     public void deleteById(long id) {
-        Optional<Task> optionalTask = taskRepository.findById(id);
+        Optional<TaskDto> optionalTask = taskRepository.findById(id).map(this::toDto);
         if (optionalTask.isEmpty()) {
             return;
         }
 
-        Task task = optionalTask.get();
+        TaskDto task = optionalTask.get();
         LocalDateTime now = LocalDateTime.now();
         if (task.getCreatedAt().plusMinutes(5).isAfter(now)) {
             throw new IllegalStateException("Невозможно удалить задачу, созданную менее 5 минут назад.");
@@ -74,5 +103,15 @@ public class TaskService {
 
     public long countActiveTasksByUserId(long userId) {
         return taskRepository.countActiveTasksByUserId(userId);
+    }
+
+    public TaskDto toDto(Task task){
+        TaskDto dto = new TaskDto();
+        dto.setId(task.getId());
+        dto.setTitle(task.getTitle());
+        dto.setStatus(task.getStatus());
+        dto.setCreatedBy(task.getCreatedByUser().getId());
+        dto.setCreatedAt(task.getCreatedAt());
+        return dto;
     }
 }
